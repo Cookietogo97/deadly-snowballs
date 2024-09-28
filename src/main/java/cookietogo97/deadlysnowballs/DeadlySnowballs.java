6e7ddd5c0f8129bc716464b4689ae31662ce1af0
Cookietogo97 <cookietogo97@gmail.com>
2024-09-29 01:16:25 +0200

    Added recipe group to DeadlySnowballs and updated version number to 1.0.1

--- src/main/java/cookietogo97/deadlysnowballs/DeadlySnowballs.java
+++ src/main/java/cookietogo97/deadlysnowballs/DeadlySnowballs.java
@@ -1,7 +1,12 @@
 package cookietogo97.deadlysnowballs;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.block.Block;
 import net.minecraft.core.data.registry.Registries;
+import net.minecraft.core.data.registry.recipe.RecipeGroup;
+import net.minecraft.core.data.registry.recipe.RecipeSymbol;
+import net.minecraft.core.data.registry.recipe.entry.RecipeEntryCrafting;
+import net.minecraft.core.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
@@ -34,6 +39,8 @@ public class DeadlySnowballs implements ModInitializer, GameStartEntrypoint, Rec
 	@Override
 	public void initNamespaces() {
 		new DeadlySnowballsRecipeRegistries();
+		final RecipeGroup<RecipeEntryCrafting<?, ?>> WORKBENCH = new RecipeGroup<>(new RecipeSymbol(new ItemStack(Block.workbench)));
+		DeadlySnowballsRecipeRegistries.DEADLYSNOWBALLS.register("workbench", WORKBENCH);
 		Registries.RECIPES.register("deadlysnowballs", DeadlySnowballsRecipeRegistries.DEADLYSNOWBALLS);
 	}
 }
